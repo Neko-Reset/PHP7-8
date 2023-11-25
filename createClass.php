c4ad81260d17c7123b1e6bdda8a8fdd146de097e
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2023-11-25 19:56:23 +0900

    ファンクション関数

--- createClass.php
+++ createClass.php
@@ -2,11 +2,17 @@
 class Item {
     public string $name; // 商品名
     public int $price;   // 価格
+
+    // 価格を取得する
+    public function getPrice(string $yen = "") :string
+    {
+        return number_format($this->price) . $yen;
+    }
 }
 
 $item = new Item();
 
 $item->name = "php";
-$item->price = 1000;
+$item->price = 150000;
 
-echo $item->name, "/", $item->price;
+echo $item->name, "/", $item->getPrice("円");

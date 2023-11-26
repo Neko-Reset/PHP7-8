73d6092c3b01c6d20cb73119044b89061f9bcfe1
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2023-11-26 22:12:37 +0900

    parent

---
+++ Parent.php
@@ -0,0 +1,33 @@
+<?php
+class Item {
+    protected int $unit; // 個数
+
+    public function __construct(int $unit) {
+        $this->unit = $unit; // 個数
+    }
+
+    public function getUnit()
+    {
+        return $this->unit;
+    }
+}
+
+class Book extends Item {
+    private int $page;
+
+    public function __construct(int $unit, int $page) {
+        // 親クラスのメソッドを呼び出す
+        parent::__construct($unit);
+        $this->page = $page;
+    }
+
+
+    // オーバーライズ
+    public function getUnit()
+    {
+        return $this->unit;  // 冊
+    }
+}
+
+$item = new Item(5);
+echo $item->getUnit();

9defda043e298cb23fa1baa339d3f5c394c7482b
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2024-02-04 19:17:25 +0900

    callメソッド

--- MagicMethod/__callStatic.php
+++ MagicMethod/__callStatic.php
@@ -1,8 +1,22 @@
 <?php
-
 /** 
  * __callStatic() は、アクセス不可能な静的メソッドが
  * 呼び出されたときに自動的に実行されるマジックメソッドです。
  * __call()がインスタンスメソッドに対して動作するのに対し
  * __callStatic()はstaticな静的メソッドの呼び出しに対して動作します。
  */
+
+class Bob
+{
+    private static function hello()
+    {
+        echo "bobです";
+    }
+
+    public static function __callStatic($method, $args)
+    {
+        echo "__callStaticメソッドが呼ばれました";
+    }
+}
+
+Bob::jonh();
